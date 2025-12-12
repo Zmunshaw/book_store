a027caffc0a61111391d93e7dc280d78ca34e0de
Zachary Munshaw <sarusian@gmail.com>
2025-12-12 15:36:44 -0500

    Fix Changes

--- frontend/lib/app_data/app_globals.dart
+++ frontend/lib/app_data/app_globals.dart
@@ -1,3 +1,3 @@
 const String appName = 'Bookify';
 const String appVersion = '1.0.0';
-const String backendUrl = 'http://localhost:8000';
+const String backendUrl = 'https://mobile-dev.zacharymunshaw.dev';
